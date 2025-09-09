2a551c80a8244db9a017506a997f524a17d3de3c
keerthana <keerthanap773@gmail.com>
2025-09-09 12:41:04 +0530

    American guy commit

--- src/apipack/RestAPIDemoGitEx.java
+++ src/apipack/RestAPIDemoGitEx.java
@@ -10,9 +10,21 @@ public class RestAPIDemoGitEx {
 		System.out.println("POST JIRA1");
 		System.out.println("POST JIRA2");
 		System.out.println("POST JIRA3");
+		//another person
+		System.out.println("POST JIRA4");
+		System.out.println("POST JIRA5");
+		System.out.println("POST JIRA6");
 		
 		
 		
+	}
+	
+	@Test
+	public void BrowserAutomation() 
+	{
+		System.out.println("Web Automation");
+		
+		System.out.println("Mobile Automation");
 	}
 	
 	@Test
