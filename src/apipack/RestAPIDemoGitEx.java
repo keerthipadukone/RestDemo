7a8b8fd5f0c5c77e64e3650698f4a8838235915f
keerthana <keerthanap773@gmail.com>
2025-09-09 12:23:20 +0530

    this commit is from Asian Timezone guy

---
+++ src/apipack/RestAPIDemoGitEx.java
@@ -0,0 +1,26 @@
+package apipack;
+
+import org.testng.annotations.Test;
+
+public class RestAPIDemoGitEx {
+	
+	@Test
+	public void postJira() 
+	{
+		System.out.println("POST JIRA1");
+		System.out.println("POST JIRA2");
+		System.out.println("POST JIRA3");
+		
+		
+		
+	}
+	
+	@Test
+	public void deletTwitter() 
+	{
+		System.out.println("Delete twitter");
+	}
+	
+}
+
+
